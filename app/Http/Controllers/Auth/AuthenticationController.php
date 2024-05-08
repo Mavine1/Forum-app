7e9f65756a84a7189d17e2f24680779b08aaf4c9
Mavine1 <mavinemalin@gmail.com>
2024-05-08 11:34:01 +0300

    property mad lady facing pig

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 class AuthenticationController extends Controller
 {
     public function register(){
-
+        return response ([
+            'message' => 'Api is working '
+        ], 200);
     }
 }
