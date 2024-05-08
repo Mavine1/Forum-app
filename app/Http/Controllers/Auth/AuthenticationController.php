087adf1b4b87b37580a89286706490810d9cbe60
Mavine1 <mavinemalin@gmail.com>
2024-05-08 12:18:00 +0300

    desert anywhere visit brought wrong

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 
 class AuthenticationController extends Controller
 {
-    public function register(RegisterListenersPass){
+    public function register(RegisterRequest $request){
         return response ([
             'message' => 'Api is working '
         ], 200);
