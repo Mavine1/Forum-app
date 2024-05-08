f854205467aa2a631d5622bf267ac1322fae5b16
Mavine1 <mavinemalin@gmail.com>
2024-05-08 12:10:01 +0300

    plural adventure bowl operation three

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -11,7 +11,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return True;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|min:3',
+            'username' => 'required|string|min:4|unique:user'
         ];
     }
 }
