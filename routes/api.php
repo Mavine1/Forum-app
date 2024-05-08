68999d2dc6178ee00e7d95c2d079651792511e05
Mavine1 <mavinemalin@gmail.com>
2024-05-08 11:02:00 +0300

    proud declared rope stay settlers

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/test',)
+Route::get('/test', function()){
+    return response ([
+        'message' => 'Api is working '
+    ])
+}
