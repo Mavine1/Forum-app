71d317e7d556bf9179a7ebfa6e9e77fe92e15864
Mavine1 <mavinemalin@gmail.com>
2024-05-08 12:40:01 +0300

    ahead prevent nor what shaking

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\models\User;
 use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 use Illuminate\Support\Facades\Hash;
 
@@ -18,6 +19,6 @@ class AuthenticationController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ];
-        $user = User::
+        $user = User::create($userData);
     }
 }
