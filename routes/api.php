63e04df39418deb7179066b1df03b08876bb5683
Mavine1 <mavinemalin@gmail.com>
2024-05-08 11:04:00 +0300

    plenty say act television choose

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/test', function()){
+Route::get('/test', function(){
     return response ([
         'message' => 'Api is working '
-    ])
-}
+    ], 200);
+});
