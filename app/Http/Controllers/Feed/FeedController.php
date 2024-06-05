8f260c500374bc710d111e908312fb7372ac3127
Mavine1 <mavinemalin@gmail.com>
2024-06-05 16:46:00 +0300

    save short foot chicken breathing

--- app/Http/Controllers/Feed/FeedController.php
+++ app/Http/Controllers/Feed/FeedController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Feed;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // Import the correct request class
 
 class FeedController extends Controller
 {
-    //
+    public function store(PostRequest $request) // Use the correct class name
+    {
+        $request->validated();
+
+        auth()->user()->feeds()->create([
+            'content' => $request->content
+        ]);
+
+        return response([
+           'message' => 'Successfully created feed'
+        ], 201);
+    }
 }
--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -20,3 +20,4 @@ class Feed extends Model
         return $this->belongsTo(User::class);
     }
 }
+
