04a54745467556eea14725967444f185bd359e65
Mavine1 <mavinemalin@gmail.com>
2024-06-05 16:34:00 +0300

    believed southern highest major depth

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Feed extends Model
 {
     use HasFactory;
-    protected $fillable = {
+
+    protected $fillable = [
         'user_id',
         'content'
-    };
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
