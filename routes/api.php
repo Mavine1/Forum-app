460b016f7a5bb2d43a97ad5eb64e23563f409b06
Mavine1 <mavinemalin@gmail.com>
2024-06-05 16:52:01 +0300

    monkey news closer greatest mistake

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticationController;
-use App\Http\Controllers\Auth\FeedController;
+use App\Http\Controllers\FeedController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
-Route::get('/test', function(){
-    return response ([
-        'message' => 'Api is working '
+Route::get('/test', function () {
+    return response([
+        'message' => 'API is working'
     ], 200);
 });
-Route::post('register',[AuthenticationController::class, 'register']);
-Route::post('login',[AuthenticationController::class, 'login']);
-Route::post('store',[FeedController::class, 'store']);
+
+Route::post('register', [AuthenticationController::class, 'register']);
+Route::post('login', [AuthenticationController::class, 'login']);
+Route::post('store', [FeedController::class, 'store'])->middleware('auth:sanctum');
